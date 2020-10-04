c03fee0ffa33096888210f20b1a0074bb8612285
DeltaNedas <deltanedas@gmail.com>
2020-10-04 22:51:22 +0100

    1.1.0: string instruction, memory bank stolen by cat

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+require("anuke-logic/string");
---
+++ scripts/string.js
@@ -0,0 +1,188 @@
+const ops = {
+	len: {
+		run: string => string.length
+	},
+
+	// result = string.substr(start, end)
+	sub: {
+		args: {start: "num", length: "num"},
+
+		run(string, args) {
+			if (args.start < 0) {
+				args.start += string.length;
+			}
+			if (args.length < 1) {
+				args.length += string.length;
+			}
+
+			return string.substr(args.start, args.length);
+		}
+	},
+
+	// result = string + add
+	add: {
+		args: {add: "obj"},
+
+		run: (string, args) => string + args.add
+	},
+
+	char: {
+		args: {index: "num"},
+
+		run(string, args) {
+			if (args.index < 0) {
+				args.index += string.length;
+			}
+
+			const code = string.charCodeAt(args.index);
+			return isNaN(code) ? -1 : code;
+		}
+	},
+
+	// result = string + Character(char)
+	push: {
+		args: {char: "num"},
+
+		run(string, args) {
+			try {
+				return string + java.lang.Character(args.char);
+			} catch (e) {
+				return string;
+			}
+		}
+	}
+};
+
+const StringI = {
+	_(builder, op, result, string, args) {
+		this.op = op;
+		this.args = {};
+
+		this.residx = builder.var(result);
+		this.stridx = builder.var(string);
+
+		const opargs = (ops[op] || {}).args;
+		if (!opargs) return;
+
+		this.indices = {};
+		const argnames = Object.keys(opargs);
+		for (var i in argnames) {
+			this.indices[argnames[i]] = builder.var(args[i]);
+		}
+	},
+
+	run(vm) {
+		const op = ops[this.op];
+		if (!op) return;
+
+		this.vm = vm;
+		this.string = "" + vm.obj(this.stridx);
+
+		for (var arg in op.args || {}) {
+			this.args[arg] = vm[op.args[arg]](this.indices[arg]);
+		}
+
+		this.setResult(op.run(this.string, this.args, this));
+	},
+
+	setResult(str) {
+		this.vm["set" + ((typeof(str) == "number") ? "num" : "obj")](this.residx, str);
+	}
+};
+
+const StringStatement = {
+	new: words => {
+		const st = extend(LStatement, Object.create(StringStatement));
+		st.read(words);
+		return st;
+	},
+
+	read(words) {
+		this.op = words[1];
+		this.result = words[2];
+		this.string = words[3];
+
+		this.args = [];
+		for (var i = 4; i < 6; i++) {
+			this.args[i - 4] = words[i];
+		}
+	},
+
+	build(h) {
+		if (h instanceof Table) {
+			return this.buildt(h);
+		}
+
+		const inst = extend(LExecutor.LInstruction, Object.create(StringI));
+		inst._(h, this.op, this.result, this.string, this.args);
+		return inst;
+	},
+
+	buildt(table) {
+		this.field(table, this.result, res => {this.result = res});
+		table.add("=");
+
+		/* dropdown for op */
+		var opb = table.button(this.op, Styles.logict, () => {
+			this.showSelectTable(opb, (t, hide) => {
+				for (var op in ops) {
+					this.setter(table, t, op, hide);
+				}
+			});
+		}).width(100).color(table.color).get();
+		this.row(table);
+
+		this.field(table, this.string, str => {this.string = str});
+
+		if (!ops[this.op]) return;
+
+		/* Op-specific args */
+		const argnames = Object.keys(ops[this.op].args || {});
+		for (var i in argnames) {
+			const idx = i;
+			if (this.args[i] === undefined) {
+				this.args[i] = argnames[i];
+			}
+
+			this.field(table, this.args[i], arg => {this.args[idx] = arg});
+
+			if ((i % 2) == 0 && i > 0) this.row(table);
+		}
+	},
+
+	setter(root, table, op, hide) {
+		table.button(op, () => {
+			this.op = op;
+			root.clearChildren();
+			hide.run();
+			this.buildt(root);
+		}).row();
+	},
+
+	write(b) {
+		b.append("string ");
+		b.append(this.op + " ");
+		b.append(this.result + " ");
+		b.append(this.string + "");
+
+		for (var i of this.args) {
+			if (i != "undefined") {
+				b.append(" " + i);
+			}
+		}
+	},
+
+	name: () => "String",
+	category: () => LCategory.operations
+};
+
+/* Mimic @RegisterStatement */
+LAssembler.customParsers.put("string", func(StringStatement.new));
+
+LogicIO.allStatements.add(prov(() => StringStatement.new([
+	"string",
+	"add",
+	"result",
+	'"frog"',
+	'" cat"'
+])));
